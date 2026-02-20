c16d8bcc0ed6cbe099ff7c3a674a79029ecba08e
gab-correia <gabrielcc3@al.insper.edu.br>
2026-02-20 09:49:50 -0300

    cppcheck

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -5,16 +5,17 @@
 
 int multiplyNumbers(int x, int y) {
     int product = x * y;
+    return product;
 }
 
-
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 5, b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
+        printf("%d", arr[i]);
     }
 
     return 0;
