0f70606c9b314c212e7486c6516ce9fdbb5c5796
gab-correia <gabrielcc3@al.insper.edu.br>
2026-02-20 09:58:20 -0300

    cppcheck

--- exe0_variables/main.c
+++ exe0_variables/main.c
@@ -1,15 +1,16 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int a = 0; // var global
-int b = 0; // var global
 
 void foo(void) {
-    a = a + 1; // acessa variavel global
+    *a = *a + 1; // acessa variavel global
 }
 
 void main(void) {
+    int a = 0; 
+    int b = 0; 
+
     while (1) {
-        foo();
+        foo(&a);
         
         if (a > 5) {
             b = 1;
