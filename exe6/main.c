388632a36f7f34bbf0379958bf51955fa046a5b5
gab-correia <gabrielcc3@al.insper.edu.br>
2026-02-23 13:46:55 -0300

    exe6

--- exe6/main.c
+++ exe6/main.c
@@ -5,20 +5,9 @@
 #define FIRST_GPIO 2
 #define BTN_PIN_G 28
 
-static int last_btn = 1; 
-static int cnt = 0;
-
 static const int bits[10] = {
-    0x3f,  // 0: ABCDEF
-    0x06,  // 1: BC
-    0x5b,  // 2: ABGED
-    0x4f,  // 3: ABCDG
-    0x66,  // 4: BCFG
-    0x6d,  // 5: ACDFG
-    0x7d,  // 6: ACDEFG
-    0x07,  // 7: ABC
-    0x7f,  // 8: ABCDEFG
-    0x67   // 9: ABCDFG
+    0x3f, 0x06, 0x5b, 0x4f, 0x66,
+    0x6d, 0x7d, 0x07, 0x7f, 0x67
 };
 
 void seven_seg_init(void) {
@@ -43,14 +32,17 @@ int main(void) {
     stdio_init_all();
     
     seven_seg_init();
-    seven_seg_display(0); 
+    seven_seg_display(0);
+    
+    int cnt = 0;
+    int last_btn = 1;
     
     while (true) {
         int btn = gpio_get(BTN_PIN_G);
-        if (last_btn && !btn) {  
+        if (last_btn && !btn) {
             cnt = (cnt + 1) % 10;
             seven_seg_display(cnt);
-            printf("cnt: %d\n", cnt);  
+            printf("cnt: %d\n", cnt);
         }
         last_btn = btn;
         sleep_ms(10);
